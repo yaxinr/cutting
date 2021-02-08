490b1a40a259552d6ccbd7b590e245973cb88b81
gilfanovrrPaker <gilfanovrr@npf-paker.ru>
2021-02-08 17:32:56 +0500

    reserve

--- Cutting/Program.cs
+++ Cutting/Program.cs
@@ -5,51 +5,51 @@ using System.Text;
 
 namespace Cutting
 {
-    class Program
+    public class Program
     {
         static void Main()
         {
-            var material1 = new Product(1);
+            var material1 = new Product("1");
             var sourceBatch1 = new Batch("plavka1", material1);
             var materialBatch1 = new Batch("te1", material1, 35, sourceBatch1);
             var sourceBatch2 = new Batch("plavka2", material1);
             var materialBatch2 = new Batch("te2", material1, 30, sourceBatch2);
             List<Batch> materialBatches = new List<Batch>() { materialBatch1, materialBatch2 };
 
-            var product = new Product(2, 10, 20, material1);
+            var product = new Product("2", 10, 20, material1.id);
             var productBatch1 = new Batch("det1", product, 1);
             var productBatch2 = new Batch("det2", product, 3);
-            var workcenter = new Workcenter();
 
-            List<Batch> productBatches = new List<Batch>() { productBatch1, productBatch2 };
+            IEnumerable<Batch> productBatches = new Batch[] { productBatch1, productBatch2 };
             PrintResult(Calc(productBatches, materialBatches));
             Console.Read();
         }
 
-        public static List<Reserve> Calc(List<Batch> productBatches, List<Batch> materialBatches)
+        public static Reserve[] Calc(IEnumerable<Batch> productBatches, IEnumerable<Batch> materialBatches)
         {
             List<Reserve> reserves = new List<Reserve>();
             var sourcesDic = materialBatches.ToLookup(x => x.sourceBatch);
-            var materialDic = sourcesDic.ToLookup(x => x.Key.product);
-            foreach (var productBatch in productBatches.OrderByDescending(b=>b.Required))
+            var materialDic = sourcesDic.ToLookup(x => x.Key.product.id);
+            foreach (var productBatch in productBatches.OrderByDescending(b => b.Required))
             {
-                foreach (var meltBatches in materialDic[productBatch.product.material].OrderByDescending(m => m.Sum(b=> b.NotReserved)))
+                foreach (var meltBatches in materialDic[productBatch.product.material].OrderByDescending(m => m.Sum(b => b.NotReserved)))
                 {
                     if (CheckMelt(meltBatches, productBatch))
                     {
                         foreach (var materialBatch in meltBatches.OrderBy(b => b.id))
-                        {
-                            var reserve = new Reserve(productBatch, materialBatch);
-                            reserves.Add(reserve);
-                        }
+                            if (materialBatch.NotReserved >= productBatch.product.len && productBatch.NotProvided >0)
+                            {
+                                var reserve = new Reserve(productBatch, materialBatch);
+                                reserves.Add(reserve);
+                            }
                         break;
                     }
                 }
             }
-            return reserves;
+            return reserves.ToArray();
         }
 
-        private static void PrintResult(List<Reserve> reserves)
+        private static void PrintResult(IEnumerable<Reserve> reserves)
         {
             //Console.WriteLine(reserves);
             foreach (var reserve in reserves)
@@ -134,6 +134,8 @@ namespace Cutting
             this.quantity = quantity;
             Reserved = 0;
             Provided = 0;
+            NotReserved = quantity;
+            NotProvided = quantity;
             this.sourceBatch = sourceBatch;
         }
 
@@ -145,16 +147,16 @@ namespace Cutting
 
     public class Product
     {
-        readonly int id;
+        public readonly string id;
         public int len;
-        int seconds;
-        public Product material;
+        public int pieces;
+        public string material;
 
-        public Product(int id, int len = 0, int seconds = 0, Product material = null)
+        public Product(string id, int len = 0, int pieces = 0, string material = null)
         {
             this.id = id;
             this.len = len;
-            this.seconds = seconds;
+            this.pieces = pieces;
             this.material = material;
         }
     }
